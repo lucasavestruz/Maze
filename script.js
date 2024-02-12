04e88fda8584a9f79e2d0bf865f06a3b57bf993f
S0ndreRV <sondrevolsdal@icloud.com>
2024-02-12 14:48:05 +0100

    e

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-button.addEventListener('click' () => {
+button.addEventListener('click'()) => {
     fargeOvergang = document.querySelector('fargeOvergang-input').value;
     selection.style.background
 
-})
+}
