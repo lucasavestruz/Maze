5a335030b0a0d46ee84e43c2638eb99b648197d1
fabianEngedal <158446285+fabianEngedal@users.noreply.github.com>
2024-02-12 15:21:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const teleport = document.querySelector('.clipPath-overgang')
+
 button.addEventListener('click'()) => {
     fargeOvergang = document.querySelector('fargeOvergang-input').value;
     selection.style.background
