9210f389cbe2a578cd2843c4d381b167099c32d3
S0ndreRV <sondrevolsdal@icloud.com>
2024-02-27 12:54:38 +0100

    hy

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const body = document.querySelector("body");
 
 port.addEventListener("click", () => {   
     portal.classList.add("aktiv");
+    const body_portal=document.querySelectorAll(".body_portal")
+    const body_maze=document.querySelectorAll(".body_maze")
     setTimeout(() => {
         body.style.backgroundColor = "black";
     }, 1200); 
+
+    setTimeout(() => {
+        body_portal.style.display = "none";
+    }, 1300); 
+
+    setTimeout(() => {
+        body_maze.style.display = "block";
+    }, 1300); 
 })
