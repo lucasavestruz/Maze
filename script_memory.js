228f2ce572f56968373375e54ff7253d7c52ccb0
S0ndreRV <sondrevolsdal@icloud.com>
2024-03-11 14:50:14 +0100

    jabadabado

--- script_memory.js
+++ script_memory.js
@@ -2,8 +2,8 @@ const kortContainer = document.querySelector(".brett")
 // Genererer 8 tilfeldige farger
 let farger = [];
 for (let i = 0; i < 8; i++) {
-  const tilfeldigFarge = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  farger.push(tilfeldigFarge);
+    const tilfeldigFarge = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    farger.push(tilfeldigFarge);
 }
 
 const fargeListe = [...farger, ...farger];
@@ -17,6 +17,7 @@ let venterAvslutning = false;
 let trekk = 0;
 
 // Lydeffekter
+/*
 function spillTrykk() {
     let trykk = new Audio('lydeffekter/trykk.mp3');
     trykk.play();
@@ -36,17 +37,17 @@ function spillSeier() {
     let seier = new Audio('lydeffekter/riktig.mp3');
     seier.play();
 }
-
+*/
 function genererKort(farge) {
     const element = document.createElement("div");
 
     element.classList.add("kort");
     element.setAttribute("info-farge", farge);
     element.setAttribute("info-avdekket", "false");
-    
+
     element.addEventListener("click", () => {
         const avdekket = element.getAttribute("info-avdekket");
-        spillTrykk()
+        //spillTrykk()
 
         if (
             venterAvslutning
@@ -55,9 +56,9 @@ function genererKort(farge) {
         ) {
             return;
         }
-        
+
         element.style.backgroundColor = farge;
-        
+
         if (!aktivtKort) {
             aktivtKort = element;
             return;
@@ -70,25 +71,29 @@ function genererKort(farge) {
         if (fargeMatch == farge) {
             aktivtKort.setAttribute("info-avdekket", "true");
             element.setAttribute("info-avdekket", "true");
-            
+
 
             aktivtKort = null;
             venterAvslutning = false;
             kortAvdekket += 2;
-            
-            if (kortAvdekket === antallKort) {
-                alert("Gratulerer! Du løste spillet på "+ trekk + " trekk! Last inn på nytt for å spille igjen.")
-                spillSeier()
+
+            if (kortAvdekket === antallKort && teller.innerHTML <= 20) {
+                alert("Gratulerer! Du løste Memory på " + trekk + " trekk! Du er videre!")
+
+                document.querySelector(".body_maze").style.display = "block";
+                document.querySelector(".body_memory").style.display = "none";
+
             }
 
+
             return;
-        
-            
+
+
         }
 
         venterAvslutning = true;
 
-        
+
         setTimeout(() => {
             element.style.backgroundColor = null;
             aktivtKort.style.backgroundColor = null;
@@ -96,15 +101,15 @@ function genererKort(farge) {
             venterAvslutning = false
             aktivtKort = null
         }, 1000);
-        
+
     })
-    
+
     return element;
-    
+
 }
 
 // Genererer kortene med JS
-for (let i = 0; i < antallKort; i ++) {
+for (let i = 0; i < antallKort; i++) {
     const indeksFarger = Math.floor(Math.random() * fargeListe.length)
     const farge = fargeListe[indeksFarger];
     const kort = genererKort(farge);
@@ -113,4 +118,15 @@ for (let i = 0; i < antallKort; i ++) {
     kortContainer.appendChild(kort);
 }
 
-//export { teller, kortAvdekket, antallKort };
+/*
+if (teller.innerHTML >= 45 && kortAvdekket === antallKort) {
+    const body_maze = document.querySelector(".body_maze");
+    const body_memory = document.querySelector(".body_memory");
+  
+   // if (body_maze && body_memory) { 
+     body_maze.style.display = "block";
+     body_memory.style.display = "none";
+    } else {
+      console.error("Elementer ikke funnet."); 
+    }
+// }*/
